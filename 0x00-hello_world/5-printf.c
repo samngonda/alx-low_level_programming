725e25e93adf7b340808bc8b11a94186e052d3ef
samngonda <samngonda@gmail.com>
2022-09-08 13:35:21 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,12 @@
 /**                                                                                                          
  * main - print the string in the put function                                                               
  *                                                                                                           
- * Description : using the main function                                                                     
+ * Description: using the main function                                                                     
  * this program prints "programming is like building a multilingual puzzle                                   
- * return : 0                                                                                                
+ * Return: 0                                                                                                
  */                                                                                                          
-int main (void)                                                                                              
+int main(void)                                                                                              
 {                                                                                                            
         print ("with proper grammer, but the outcome is a piece of art,\n");                                 
         return (0);                                                                                          
 } 
-
