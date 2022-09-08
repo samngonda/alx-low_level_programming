9bfb86c9a0ec491ca508f52f927bfc6e9f224d53
samngonda <samngonda@gmail.com>
2022-09-08 13:17:39 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
+#include <stdio.h>                                                                                           
+                                                                                                             
+/**                                                                                                          
+ * main - print the string in the put function                                                               
+ *                                                                                                           
+ * Description : using the main function                                                                     
+ * this program prints "programming is like building a multilingual puzzle                                   
+ * return : 0                                                                                                
+ */                                                                                                          
+int main (void)                                                                                              
+{                                                                                                            
+        print ("with proper grammer, but the outcome is a piece of art,\n");                                 
+        return (0);                                                                                          
+} 
 
-/**
- * main - print the string in the put function
- *
- * Description : using the main function
- * this program prints "programming is like building a multilingual puzzle 
- * return : 0
- */
-int main (void)
-{
-	print ("with proper grammer, but the outcome is a piece of art,\n");
-	return (0);
-}
