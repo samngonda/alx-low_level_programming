81234e43b879a692d7df03b67d217e25b474726b
samngonda <samngonda@gmail.com>
2022-09-08 13:08:53 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - print the string in the put function
  *
@@ -11,4 +12,3 @@ int main (void)
 	print ("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
-
