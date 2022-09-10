068afcd564b208fbf01a95d9dc4019a044f22e1b
samngonda <samngonda@gmail.com>
2022-09-10 15:17:50 -0700

    0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,32 @@
-#include <stdio.h>                                                                                                               
-#include <stdlib.h>                                                                                                              
-#include <time.h>                                                                                                                
-/**                                                                                                                              
- * main - Entry Point                                                                                                            
- *                                                                                                                               
- * Return: Always success                                                                                                        
- */                                                                                                                              
-int main(void)                                                                                                                   
-{         
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print if the number is postive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
+int main(void)
+{
 int n;
 
-srand(time(0));                                                                                                                  
-n = rand() - RAND_MAX / 2;                                                                                                       
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
 if (n > 0)
-{       	
-	printf("%d is positive\n", n);
-}	
+{
+	printf("%i is positive\n", n);
+}
 else if (n == 0)
-{	
-        printf("%d is zero\n", n);
-}	
+{
+	printf("%i is zero\n", n);
+}
 else if (n < 0)
 {
-        printf("%d is negative\n", n);
-}	
-return (0);                                                                                                                      
-} 
+	printf("%i is negative\n", n);
+}
+return (0);
+}
