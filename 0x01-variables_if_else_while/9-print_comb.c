f3eb8ea22c2b9766a472efa00be86b5dbac03b1e
samngonda <samngonda@gmail.com>
2022-09-11 14:26:44 -0700

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,14 @@
  */
 int main(void)
 {
-		int c;
+	int c;
 
-		for (c = 48; c <= 57; c++)
-		{
+	for (c = 48; c <= 57; c++)
+	{
 		putchar(c);
 		if (c != 57)
-		{
-                																	putchar(',');
-        																		putchar(' ');
+		{      	  																putchar(',');
+	         	putchar(' ');
 		}
 	}
 	putchar('\n');
