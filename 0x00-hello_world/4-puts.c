ead6ae0c291798d3c88a2f914676cd3d8cbaec17
samngonda <samngonda@gmail.com>
2022-09-12 14:46:52 -0700

    puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,11 @@
 * main - print the string in the put function
 *
 * Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle"
+* this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
 {
-         puts("\"programming is like builiding a multilingual puzzle");
+       	puts("\"programming is like builiding a multilingual puzzle");
 	 return (0);
 }
