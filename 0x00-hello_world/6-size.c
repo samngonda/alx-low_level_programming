856e7ee05e065ac91ca70a5c30fd8a0c07b87d9c
samngonda <samngonda@gmail.com>
2022-09-12 15:42:07 -0700

    size c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,18 +7,18 @@
 * this program prints "programming is like building a multilingual puzzle
 * Return: 0
 */
-int main(void) 
-{ 
-	char a; 
-	int b; 
-	long int c; 
-	long long int d; 
-	float e; 
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(a)); 
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
 	printf("Size of an int: %d byte(s)\n", sizeof(b)); 
 	printf("Size of a long int: %d byte(s)\n", sizeof(c)); 
 	printf("Size of a long long int: %d byte(s)\n", sizeof(d)); 
-	printf("Size of a float: %d byte(s)\n", sizeof(e)); 
-	return (0); 
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
+	return (0);
 }
