a6dbb296b63683dcc352fb9c341aee7ed2c98f90
samngonda <samngonda@gmail.com>
2022-09-12 04:50:35 -0700

    size c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,12 @@
 #include <stdio.h> 
 
 /** 
- *main - Prints the sizeof many var types 
- *
- *Return: Always 0 (Success) 
- */ 
+* main - Print the string in the put function 
+*
+* Description: using the main function
+* this program prints "programming is like building a multilingual puzzle
+* return 0
+*/ 
 int main(void) 
 { 
 	char a; 
