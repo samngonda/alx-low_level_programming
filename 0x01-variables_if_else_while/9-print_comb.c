2ce8bf0f1887d8f3364eab550fe1f7a7d7f331f3
samngonda <samngonda@gmail.com>
2022-09-12 12:50:01 -0700

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,8 +13,10 @@ int main(void)
 	{
 		putchar(c);
 		if (c != 57)
-         	{  
-			putchar(',');         							        putchar(' ');   	        					}
+         	{
+	         	putchar(',');  
+           		putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
