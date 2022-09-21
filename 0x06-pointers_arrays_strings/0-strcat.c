ece300743a462b4b8e0f82822698ef6cd1176874
samngonda <samngonda@gmail.com>
2022-09-21 14:16:41 -0700

    Write a function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,19 +4,19 @@
  *strcat - Concatenates the string pointed to by @src, including the terminat
  *	 null byte, to the end of the string pointed to by @dest.
  *@dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
+ *@src: The source string to be appended to @dest.
 
- * Return: A pointer to the destination string @dest.
+ *Return: A pointer to the destination string @dest.
  */
 char *strcat(char *dest, const char *src)
 {
-       	int index = 0, dest_len = 0;
-       
+	int index = 0, dest_len = 0;
+	
 	while (dest[index++])
-		dest_len+;
-       
+		dest_len++;
+	
 	for (index = 0; src[index]; index++)
-	       	dest[dest_len++] = src[index];
+		dest[dest_len++] = src[index];
        
 	return (dest);
 }
