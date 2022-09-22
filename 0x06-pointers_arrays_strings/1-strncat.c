7aa5ab26ae30dcd96feafc486cad50971b74d9d3
samngonda <samngonda@gmail.com>
2022-09-22 13:31:45 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,5 @@
-#include "main.h"                                                                                                                
-/**                                                                                                                              
+#include "main.h"
+/**
  * _strncat - concatenates two strings,
  * @dest: destination.
  * @src: source.
@@ -9,14 +9,11 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	
 	int index = 0, dest_len = 0;
-	
+
 	while (dest[index++])
-	       	dest_len++;
-       
+		dest_len++;
 	for (index = 0; src[index] && index < n; index++)
-	       	dest[dest_len++] = src[index];
-	
+		dest[dest_len++] = src[index];
 	return (dest);
 }
