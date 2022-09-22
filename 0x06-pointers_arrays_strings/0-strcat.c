3c41cbed353726c859715704575826523589cf57
samngonda <samngonda@gmail.com>
2022-09-22 12:00:29 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,11 @@
 char *_strcat(char *dest, char *src)
 {
 	
-	int index = 0, dest_len = 0;
+	0-strcat.c int index = 0, dest_len = 0;
 	
 	while (dest[index++]
 		dest_len++;
-
+		
 	for (index = 0; src[index]; index++
 		dest[dest_len++] = src[index];
 		
