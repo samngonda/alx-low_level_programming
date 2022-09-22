3a014e81e1514ce03c0678cca1ba521e0e0b84b8
samngonda <samngonda@gmail.com>
2022-09-22 15:30:46 -0700

    Tast 6

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,9 +11,9 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
-	
-	while (dest[index++])
+	int index = 0, dest_len = 0; dest
+
+		while (dest[index++])
 		dest_len++;
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
