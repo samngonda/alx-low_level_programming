6cafcb6a9d42f8a3bddec1752b0794c2f3af8f9d
samngonda <samngonda@gmail.com>
2022-09-22 15:44:31 -0700

    Tast 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *   _strcmp - Function which compare two strings and
  *@s1: first string
@@ -36,4 +37,5 @@ int _strcmp(char *s1, char *s2)
 		else
 			i++;
 	}
+	return (diff);
 }
