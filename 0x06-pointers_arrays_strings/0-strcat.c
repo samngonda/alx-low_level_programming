ab6abdcb35aee99c2ec557af26e8792f03622768
samngonda <samngonda@gmail.com>
2022-09-22 11:49:46 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,15 +8,16 @@
  *
  * *  Return: returns value of dest
  */
+
 char *_strcat(char *dest, char *src)
 {
 	
 	int index = 0, dest_len = 0;
 	
-	while (dest[index++];
+	while (dest[index++]
 		dest_len++;
 
-	for (index = 0; src[index]; index++;
+	for (index = 0; src[index]; index++
 		dest[dest_len++] = src[index];
 		
 	return (dest);
