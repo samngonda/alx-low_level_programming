3fac4bf5cadfbfebf61d5fede08709903c93d7f6
samngonda <samngonda@gmail.com>
2022-10-03 05:01:19 -0700

    Tast 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - prints all arguments it recieves
- * @argc: number of arguments passed to the function
- * @argv: argument vector of pointers to strings
- *
- * Return: always 0
+ * main - program that prints its name, followed by a new line
+ * @argc: argument that counts argument input
+ * @argv: argument that stores the strings in an array of char* (strings)
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	int num_1, num_2, mul;
 
-	for (i = 0; i < argc; i++)
+	if (argc != 3)
+		printf("Error\n");
+	else
 	{
-		printf("%s\n", argv[i]);
+		num_1 = atoi(argv[1]);
+		num_2 = atoi(argv[2]);
+		mul = num_1 * num_2;
+		printf("%d\n", mul);
 	}
 	return (0);
 }
-
