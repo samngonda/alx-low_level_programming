823b4b2153ac0e4573b2044806dcaefa49bfa27b
samngonda <samngonda@gmail.com>
2022-10-13 14:19:31 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -21,7 +21,31 @@ int op_add(int a, int b)
 
 int op_sub(int a, int b)
 {
+	return (a - b);
+}
+
+/**
+ * op_mul - calculates the multiplication of two numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: multiplication of a and b
+ */
+
+int op_mul(int a, int b)
+{
 	return (a * b);
+}
+
+/**
+ * op_div - calculates the division of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: divison of a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
 }
 
 /**
@@ -34,4 +58,4 @@ int op_sub(int a, int b)
 int op_mod(int a, int b)
 {
 	return (a % b);
-}
+}	
